fe367952fd448e7852727d3b81c08fbd59e4dc23
Souza11 <guisouza.c@outlook.com>
2023-05-03 23:53:51 -0300

    Interfaces Repository e Migrations com flyway

--- API/src/main/java/Med/voll/API/controller/MedicoController.java
+++ API/src/main/java/Med/voll/API/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package Med.voll.API.controller;
 
 import Med.voll.API.medico.DadosCadastroMedico;
+import Med.voll.API.medico.Medico;
+import Med.voll.API.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicoController {
 
 
+    @Autowired
+    private MedicoRepository repository;
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody DadosCadastroMedico dados){
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 
 }
---
+++ API/src/main/java/Med/voll/API/medico/MedicoRepository.java
@@ -0,0 +1,9 @@
+package Med.voll.API.medico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository extends JpaRepository<Medico,Long> {
+
+
+
+}
